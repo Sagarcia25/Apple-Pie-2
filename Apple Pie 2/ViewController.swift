dfab62e1be633625f8e8af316bf2091c7d076856
Salvador <Sagarcia2713@gmail.com>
2021-08-10 20:38:45 -0500

    Started cconnecting IBOutlets to the view controller for the tree image, guessed word label, score label, and collection of buttons.

--- Apple Pie 2/ViewController.swift
+++ Apple Pie 2/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
